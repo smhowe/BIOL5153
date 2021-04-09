fd627390fa443ac8b60a238bb906ebf6eaf642bd
Samantha Howe <smhowe@uark.edu>
2021-04-08 21:56:53 -0500

    parse_gff.py updated to extract sequence information for each feature in a .gff file

--- parse_GFF.py
+++ parse_GFF.py
@@ -3,6 +3,7 @@
 import csv
 import argparse
 from Bio import SeqIO
+
 # this script will parse a GFF file and extract each feature from the genome
 # inputs: 1) GFF file, 2) corresponding genome sequence (FASTA format)
 
@@ -13,29 +14,31 @@ parser = argparse.ArgumentParser(description= 'this script will parse a GFF file
 parser.add_argument("gff", help= 'name of gff file')
 parser.add_argument("fasta", help= 'name of fasta file')
 
-#  parse the arguments
+# parse the arguments
 args= parser.parse_args()
 
 # read in the fasta file
 genome = SeqIO.read(args.fasta, 'fasta')
-print(genome.id)
-print(genome.seq)
+# print(genome.id) - prints watermelon
+# print(genome.seq) - prints the entire sequence 
 
 # open and read in the GFF file
 with open(args.gff, 'r') as gff_in:
     # create a csv reader object
     reader = csv.reader(gff_in, delimiter='\t')
     
-    # loop over a;; lines in reader (ie parsed file)
+    # loop over lines in reader (ie parsed file)
     for line in reader:
-        start  = line[3]
-        end    = line[4]
-        strand = line[6]
-        # print(start)
-        # print(line[3], line[4])
-
-        #extract the sequence
-        print(len(genome.seq))
-
-# parse the GFF file
-
+        start  = int(line[3])-1 # extracts the start position and converts to integer, -1 ensures the first nucleotide is included
+        end    = int(line[4])   # extracts the end position and converts to integer
+        strand = line[6]        # extracts the which strand the feature is on (ie + or -)
+        info   = line[8]        # extracts the information needed for header
+        rev    = genome.seq.reverse_complement() # creates the reverse complement of the sequence 
+        
+        # print the fasta header 
+        print('>' + genome.id, info)
+       
+        # extract sequence
+        if strand == '-': #if the strand is -
+            print(rev[start:end]) # print the sequence from the rev variable according to start and end coordinates
+        else: print(genome.seq[start:end]) # if strand is not - (ie. +) print the sequnece accoring to start and end coordinates
